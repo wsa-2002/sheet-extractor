4cb58955b1e05fac8b0aed01d8c5709e3add6f2f
王紹安 <wsa@wangshaoandeMacBook-Air.local>
2022-09-13 10:24:45 +0800

    Remove useless import and modify container name

--- processor/http/public.py
+++ processor/http/public.py
@@ -1,8 +1,5 @@
-import asyncio
-
 from fastapi import APIRouter, responses, Depends, Request
 
-import log
 from middleware.headers import get_auth_token
 from service.extractor import SheetExtractor
 from persistence.line import LineHandler
@@ -21,16 +18,19 @@ async def default_page():
 
 @router.post("/callback")
 async def callback(request: Request):
-    log.info("get request")
-    # signature = request.headers['X-Line-Signature']
     body = await request.body()
     reply_token = url.get_reply_token_from_request(body.decode())
+
+    if not reply_token:
+        return
+
     video_url = url.get_url_from_request(body.decode())
     if not validators.youtube_url(video_url):
         LineHandler.send_message(reply_token=reply_token,
                                  raw_message='Invalid url, please resend.')
+
     with SheetExtractor(url=video_url) as s3_file:
         sign_url = s3_handler.sign_url(bucket=s3_file.bucket, key=s3_file.key, filename=f"{s3_file.uuid}.pdf")
+
     LineHandler.send_message(reply_token=reply_token,
                              raw_message=sign_url)
-    pass
--- utils/url.py
+++ utils/url.py
@@ -3,10 +3,15 @@ import json
 
 def get_url_from_request(request: str):
     request = json.loads(request)
-    print(request['events'])
-    return request['events'][0]['message']['text']
+    try:
+        return request['events'][0]['message']['text']
+    except IndexError:
+        return None
 
 
 def get_reply_token_from_request(request: str):
     request = json.loads(request)
-    return request['events'][0]['replyToken']
+    try:
+        return request['events'][0]['replyToken']
+    except IndexError:
+        return None
